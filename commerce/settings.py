f2680db60a1cd23ae2b4c56968cf130b8f63e578
odakris <christopher_denis@live.fr>
2023-12-04 22:11:53 +0100

    Add DEFAULT_AUTO_FILED to settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Configure DEFAULT_AUTO_FIELD
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
