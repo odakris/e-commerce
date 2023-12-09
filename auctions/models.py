7b3622610f9fdee6286f801e5ce9070c06cb7994
odakris <christopher_denis@live.fr>
2023-12-09 21:30:28 +0100

    Add Category and Auctions models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class Auction(models.Model):
+    title = models.CharField(max_length=32)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    description = models.CharField(max_length=1200)
