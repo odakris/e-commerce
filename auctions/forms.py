91f1dbfb39d50f3cfdd6fcdf896943f27023a73c
odakris <christopher_denis@live.fr>
2023-12-11 00:52:35 +0100

    Update SellForm

--- auctions/forms.py
+++ auctions/forms.py
@@ -3,11 +3,11 @@ from django import forms
 from . import utils
 
 
-class SellForm(forms.Forms):
+class SellForm(forms.Form):
     title = forms.CharField(label="Title", required=True, widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Title", "autocomplete": "off"}))
     category = forms.ChoiceField(label="Category", choices=utils.all_categories())
-    description = forms.Textarea(label="Description", required=True)
-    upload = forms.FileField(label="Image", upload_to=utils.user_directory_path)
+    description = forms.CharField(label="Description", required=True, widget=forms.Textarea())
+    upload = forms.FileField(label="Image")
     #  title = models.CharField(max_length=32)
     # category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
     # description = models.TextField(max_length=1200)
