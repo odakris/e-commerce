aa2dab18ed529ebe67b788a56b9a7127ba1a7107
odakris <christopher_denis@live.fr>
2023-12-11 00:54:16 +0100

    Update user_path_directory path

--- auctions/utils.py
+++ auctions/utils.py
@@ -2,7 +2,12 @@ from . import models
 
 def user_directory_path(instance, filename):
     # fill will be uploaded to MEDIA_ROOT/user_<id>/<filemane>
-    return 'user_{0}/{1}'.format(instance.seller.id, filename)
+    return 'user_{0}_{1}/item_{2}/{4}'.format(instance.seller.pk, instance.seller, instance.title, filename)
 
 def all_categories():
-    return models.Category.objects.all()
+    choices = []
+    for category in models.Category.objects.all():
+        choices.append((category.pk, category.name))
+
+    print(f"CHOICES: {choices}")
+    return choices
