d2b5dc50b638cf509d814558924cea8a973205d6
odakris <christopher_denis@live.fr>
2023-12-10 23:34:52 +0100

    create forms.py

---
+++ auctions/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from . import utils
+
+
+class SellForm(forms.Forms):
+    title = forms.CharField(label="Title", required=True, widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Title", "autocomplete": "off"}))
+    category = forms.ChoiceField(label="Category", choices=utils.all_categories())
+    description = forms.Textarea(label="Description", required=True)
+    upload = forms.FileField(label="Image", upload_to=utils.user_directory_path)
+    #  title = models.CharField(max_length=32)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    # description = models.TextField(max_length=1200)
+    # upload = models.FileField(upload_to=utils.user_directory_path)
+    # seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
