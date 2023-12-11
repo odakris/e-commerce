9c79e96f34ece4e68fcc8c933944da6fad30d0d2
odakris <christopher_denis@live.fr>
2023-12-12 00:29:20 +0100

    Update SellForm

--- auctions/forms.py
+++ auctions/forms.py
@@ -4,12 +4,8 @@ from . import utils
 
 
 class SellForm(forms.Form):
-    title = forms.CharField(label="Title", required=True, widget=forms.TextInput(attrs={"class":"form-control", "placeholder": "Title", "autocomplete": "off"}))
-    category = forms.ChoiceField(label="Category", choices=utils.all_categories())
-    description = forms.CharField(label="Description", required=True, widget=forms.Textarea())
-    upload = forms.FileField(label="Image")
-    #  title = models.CharField(max_length=32)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
-    # description = models.TextField(max_length=1200)
-    # upload = models.FileField(upload_to=utils.user_directory_path)
-    # seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
+    title = forms.CharField(label="TITLE", required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Title", "autocomplete": "off"}))
+    category = forms.ChoiceField(label="CATEGORY", required=True, choices=utils.all_categories())
+    description = forms.CharField(label="DESCRIPTION", required=True, widget=forms.Textarea(attrs={"placeholder": "Write a detailed description of your item"}))
+    upload = forms.FileField(label="PHOTOS", required=True, widget=forms.FileInput(attrs={"class": "file-btn", "accept": "image/*"}))
+    # PRICING
