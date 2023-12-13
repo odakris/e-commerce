85cc25568bcfaf735b755b3d38f0077f613e052a
odakris <christopher_denis@live.fr>
2023-12-13 23:58:56 +0100

    add ImagesUpload into Django database

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Category, Auction
+from .models import User, Category, Auction, ImagesUpload
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Auction)
+admin.site.register(ImagesUpload)
