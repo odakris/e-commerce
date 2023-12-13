0ef6b547f31154e2e7c4edfdffc66e4efa65af2e
odakris <christopher_denis@live.fr>
2023-12-13 23:48:16 +0100

    Fixed bug about media dir

--- auctions/utils.py
+++ auctions/utils.py
@@ -2,12 +2,11 @@ from . import models
 
 def user_directory_path(instance, filename):
     # fill will be uploaded to MEDIA_ROOT/user_<id>/<filemane>
-    return 'user_{0}_{1}/item_{2}/{4}'.format(instance.seller.pk, instance.seller, instance.title, filename)
+    return 'user_{0}_{1}/item_{2}/{3}'.format(instance.seller.pk, instance.seller, instance.title, filename)
 
 def all_categories():
     choices = []
     for category in models.Category.objects.all():
         choices.append((category.pk, category.name))
 
-    print(f"CHOICES: {choices}")
     return choices
