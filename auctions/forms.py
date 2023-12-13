beaa68d1104d42a65596bc732d22f41159f6d5fa
odakris <christopher_denis@live.fr>
2023-12-13 23:46:40 +0100

    Remove upload field from SellForm

--- auctions/forms.py
+++ auctions/forms.py
@@ -2,10 +2,9 @@ from django import forms
 
 from . import utils
 
-
 class SellForm(forms.Form):
     title = forms.CharField(label="TITLE", required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Title", "autocomplete": "off"}))
     category = forms.ChoiceField(label="CATEGORY", required=True, choices=utils.all_categories())
     description = forms.CharField(label="DESCRIPTION", required=True, widget=forms.Textarea(attrs={"placeholder": "Write a detailed description of your item"}))
-    upload = forms.FileField(label="PHOTOS", required=True, widget=forms.FileInput(attrs={"class": "file-btn", "accept": "image/*"}))
+    # upload = forms.FileField(label="PHOTOS", required=True, widget=forms.FileInput(attrs={"class": "file-btn", "accept": "image/*"}))
     # PRICING
