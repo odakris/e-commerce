ed446d122522b47272d72de771c48853bd6ddd4f
odakris <christopher_denis@live.fr>
2023-12-18 00:05:06 +0100

    Update SellForm: Add starting_bid

--- auctions/forms.py
+++ auctions/forms.py
@@ -6,5 +6,4 @@ class SellForm(forms.Form):
     title = forms.CharField(label="TITLE", required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Title", "autocomplete": "off"}))
     category = forms.ChoiceField(label="CATEGORY", required=True, choices=utils.all_categories())
     description = forms.CharField(label="DESCRIPTION", required=True, widget=forms.Textarea(attrs={"placeholder": "Write a detailed description of your item"}))
-    # upload = forms.FileField(label="PHOTOS", required=True, widget=forms.FileInput(attrs={"class": "file-btn", "accept": "image/*"}))
-    # PRICING
+    starting_bid = forms.DecimalField(label="STARTING PRICE", required=True, widget=forms.NumberInput(attrs={"min": 1, "class": "form-control", "placeholder": "$", "autocomplete": "off"}))
