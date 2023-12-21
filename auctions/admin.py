31c4c1f63d601a288e7d32d8cf197330187eced6
odakris <christopher_denis@live.fr>
2023-12-21 16:19:40 +0100

    Add Bid model

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import User, Category, Auction, ImagesUpload
+from .models import User, Category, Auction, ImagesUpload, Bid
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Auction)
 admin.site.register(ImagesUpload)
+admin.site.register(Bid)
