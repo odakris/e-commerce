38124fac096dbd6229ce67e8166cac7e5604119c
odakris <christopher_denis@live.fr>
2023-12-22 22:48:21 +0100

    Update paths

--- auctions/urls.py
+++ auctions/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = "auctions"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("categories", views.categories, name="categories"),
-    path("sell", views.sell, name="sell"),
-    path("auction/<int:auction_id>", views.auction, name="auction"),
-    path("category/<str:filter>", views.filter, name="filter")
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("category/", views.categories, name="categories"),
+    path("sell/", views.sell, name="sell"),
+    path("auction/<int:auction_id>/", views.auction, name="auction"),
+    path("category/<str:filter>/", views.filter, name="filter")
 ]
