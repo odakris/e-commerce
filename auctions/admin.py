585a7cd704679aaf0ce6b741540e9404b86e6cc2
odakris <christopher_denis@live.fr>
2024-01-05 00:11:18 +0100

    Add Comment model to admin database management

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Category, Auction, ImagesUpload, Bid, Wishlist
+from .models import User, Category, Auction, ImagesUpload, Bid, Wishlist, Comment
 
 # Register your models here.
 admin.site.register(User)
@@ -9,3 +9,4 @@ admin.site.register(Auction)
 admin.site.register(ImagesUpload)
 admin.site.register(Bid)
 admin.site.register(Wishlist)
+admin.site.register(Comment)
